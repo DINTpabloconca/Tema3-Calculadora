037b144c00cc36a0f3cf53c7e88d1472d0cea8a7
pabloconca <pabloconcasanchez@gmail.com>
2022-10-19 19:55:01 +0200

    Botones dinamicos

--- Tema3-Calculadora/MainWindow.xaml.cs
+++ Tema3-Calculadora/MainWindow.xaml.cs
@@ -23,17 +23,40 @@ namespace Tema3_Calculadora
         public MainWindow()
         {
             InitializeComponent();
+            botones();
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            String boton = (sender as Button).Tag.ToString();
-            resultadoTextBox.Text = String.Concat(resultadoTextBox.Text,boton);
-        }
+            Button boton = (Button)sender;
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+            resultadoTextBox.Text += boton.Tag.ToString();
+        }
+        private void botones()
         {
+            int val = 1;
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Button boton = new Button();
+                    Viewbox viewbox = new Viewbox();
+                    TextBlock textblock = new TextBlock();
 
+                    textblock.Text = val.ToString();
+                    boton.Tag = val.ToString();
+
+                    viewbox.Child = textblock;
+                    boton.Content = viewbox;
+
+                    Grid.SetRow(boton, i);
+                    Grid.SetColumn(boton, j);
+
+                    contenedor.Children.Add(boton);
+                    val++;
+                }
+            }
         }
+ 
     }
 }
+
